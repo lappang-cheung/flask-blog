866e33f104dc2e44e7dfda4f828ed567428db5d7
Leo Cheung <lappangcheung@gmail.com>
2020-12-25 15:46:03 -0500

    Update the email and user name

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
+# Register Form
 class RegistrationForm(FlaskForm):
     username = StringField('Username', 
         validators=[DataRequired(), Length(min=2, max=20)])
@@ -30,7 +32,7 @@ class RegistrationForm(FlaskForm):
         if user:
             raise ValidationError('That email already taken!')
 
-
+# Login Form
 class LoginForm(FlaskForm):
     email = StringField('Email', 
         validators=[DataRequired(), Email()])
@@ -41,3 +43,27 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
 
     submit = SubmitField('Login')
+
+# Update form
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', 
+        validators=[DataRequired(), Length(min=2, max=20)])
+
+    email = StringField('Email', 
+        validators=[DataRequired(), Email()])
+
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            # Check if user exist
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username already taken!')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            # Check if email exist
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email already taken!')
